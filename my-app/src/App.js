33b339a7fbee9abfa6671813fc6f079eef03ea19
hilmitekin82 <hilmi.tekin1@gmail.com>
2018-01-02 15:11:24 +0300

    cex io added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 class Tile extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { btsBt:1 , btsPr:1, krkPr:1, bfnPr:1, krkBt:1 , bfnBt:1, krkBtEth:1 };  
+    this.state = { btsBt:1 , btsPr:1, bfnPr:1, bfnBt:1, 
+      btsCexXrpUsd: 1, btsCexXrpEur:1 , btsCexBtcUsd: 1, btsCexBtcEur: 1 ,
+      bfnCexXrpUsd: 1, bfnCexBtcUsd: 1, bfnCexBtcEur: 1 
+      };  
   }
   
   componentDidMount() {
@@ -18,11 +21,40 @@ class Tile extends React.Component {
       this.getBtcTurkBTCTRY(result).then(ratedBtcTurk => {
         this.getBtcTurkETHTRY(result).then(ratedBtcTurkEth => {
           this.getParibuBTCTRY(result).then(ratedParibu => {
-            this.getBtsArb(ratedBtcTurk, ratedParibu).then(btsArb => {
-              this.getKrkArb(ratedBtcTurk, ratedParibu).then(krkArb => {
-                this.getKrkArbEth(ratedBtcTurkEth, ratedBtcTurkEth).then(krkArbEth => {
-                  this.getBfnArb(ratedBtcTurk, ratedParibu).then(bfnArb => {
-                    this.setState({ btsBt: btsArb.ab, krkBt: krkArb.ab, bfnBt: bfnArb.ab, btsPr: btsArb.ap, krkPr: krkArb.ap, bfnPr: bfnArb.ap, krkBtEth: krkArbEth.ab });
+            this.getCexBTCEUR().then(cexBtcEur => {
+              this.getCexBTCUSD().then(cexBtcUsd => {
+                this.getCexXRPEUR().then(cexXrpEur => {
+                  this.getCexXRPUSD().then(cexXrpUsd => {
+                    this.getBtsArb(ratedBtcTurk, ratedParibu, cexBtcUsd).then(btsArb => {
+                      this.getBfnArb(ratedBtcTurk, ratedParibu, cexBtcUsd).then(bfnArb => {
+                        this.getBtsBtcEurArb(cexBtcEur).then(btsBtcEurArb => {
+                          this.getBtsXrpEurArb(cexXrpEur).then(btsXrpEurArb => {
+                            this.getBtsXrpUsdArb(cexXrpUsd).then(btsXrpUsdArb => {
+                              this.getBfnBtcEurArb(cexBtcEur).then(bfnBtcEurArb => {
+                                this.getBfnXrpUsdArb(cexXrpUsd).then(bfnXrpUsdArb => {
+                                  
+
+                              this.setState({ btsBt: btsArb.ab, 
+                                bfnBt: bfnArb.ab, 
+                                btsPr: btsArb.ap,  
+                                bfnPr: bfnArb.ap,
+                                btsCexBtcUsd: btsArb.cex,
+                                bfnCexBtcUsd: bfnArb.cex,
+                                btsCexXrpEur: btsXrpEurArb.cex,
+                                btsCexBtcEur: btsBtcEurArb.cex,
+                                btsCexXrpUsd: btsXrpUsdArb.cex,
+                                bfnCexXrpUsd: bfnXrpUsdArb.cex,
+                                bfnCexBtcEur: bfnBtcEurArb.cex
+                              });
+
+
+                                });
+                              });
+                            });
+                          });
+                        });
+                      });
+                    });
                   });
                 });
               });
@@ -37,28 +69,15 @@ class Tile extends React.Component {
     setTimeout(() => {
         this.tickArb();
         this.tickPrice();
-    }, 5000); //tick every 500 - 1500 ms
+    }, 10000); //tick every 500 - 1500 ms
   }
   
-  getBfnArb(rateB, rateP) {
+  getBfnArb(rateB, rateP, rateC) {
     return fetch('https://api.bitfinex.com/v2/ticker/tBTCUSD')
       .then((response) => response.json())
       .then((responseJson) => {
         const price = responseJson[2];
-        const result = { 'ab' : ((rateB / price) - 1) * 100, 'ap' : ((rateP / price) - 1) * 100 };  
-        return result;
-    })
-      .catch((error) => {
-      console.error(error);
-    });
-  }
-  
-  getKrkArb(rateB, rateP) {
-    return fetch('https://cors-anywhere.herokuapp.com/'+'https://api.kraken.com/0/public/Ticker?pair=XBTUSD')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        const price = responseJson.result.XXBTZUSD.a[0];
-        const result = { 'ab' : ((rateB / price) - 1) * 100, 'ap' : ((rateP / price) - 1) * 100 };  
+        const result = { 'ab' : ((rateB / price) - 1) * 100, 'ap' : ((rateP / price) - 1) * 100 , 'cex' : ((rateC / price) - 1) * 100 };  
         return result;
     })
       .catch((error) => {
@@ -66,28 +85,84 @@ class Tile extends React.Component {
     });
   }
 
-  getKrkArbEth(rateB, rateP) {
-    return fetch('https://cors-anywhere.herokuapp.com/'+'https://api.kraken.com/0/public/Ticker?pair=ETHUSD')
+  getBfnBtcEurArb(rateC) {
+    return fetch('https://api.bitfinex.com/v2/ticker/tBTCEUR')
       .then((response) => response.json())
       .then((responseJson) => {
-        const price = responseJson.result.XETHZUSD.a[0];
-        const result = { 'ab' : ((rateB / price) - 1) * 100, 'ap' : ((rateP / price) - 1) * 100 };  
+        const price = responseJson[2];
+        const result = { 'cex' : ((rateC / price) - 1) * 100 };  
         return result;
     })
       .catch((error) => {
       console.error(error);
     });
   }
+
+  getBfnXrpUsdArb(rateC) {
+    return fetch('https://api.bitfinex.com/v2/ticker/tXRPUSD')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        const price = responseJson[2];
+        const result = { 'cex' : ((rateC / price) - 1) * 100 };  
+        return result;
+    })
+      .catch((error) => {
+      console.error(error);
+    });
+  }
+ 
   
-  getBtsArb(rateB, rateP) {
+  getBtsArb(rateB, rateP, rateC) {
     return fetch('https://www.bitstamp.net/api/ticker')
       .then((response) => response.json())
       .then((responseJson) => {
         const price = responseJson.ask;
-        const result = { 'ab' : ((rateB / price) - 1) * 100, 'ap' : ((rateP / price) - 1) * 100 };  
+        const result = { 'ab' : ((rateB / price) - 1) * 100, 'ap' : ((rateP / price) - 1) * 100, 'cex' : ((rateC / price) - 1) * 100 };  
         return result;
     })
       .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  getBtsBtcEurArb(rateC) {
+    return fetch('https://cors-anywhere.herokuapp.com/https://www.bitstamp.net/api/v2/ticker/BTCEUR/')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        const price = responseJson.ask;
+        const result = { 'cex' : ((rateC / price) - 1) * 100 };  
+        return result;
+    })
+      .catch((error) => {
+        return { 'cex' : ((rateC / 1) - 1) * 100 }; 
+      console.error(error);
+    });
+  }
+
+  getBtsXrpEurArb(rateC) {
+    return fetch('https://cors-anywhere.herokuapp.com/https://www.bitstamp.net/api/v2/ticker/XRPEUR/')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        const price = responseJson.ask;
+        const result = { 'cex' : ((rateC / price) - 1) * 100 };  
+        return result;
+    })
+      .catch((error) => {
+        return { 'cex' : ((rateC / 1) - 1) * 100 }; 
+      console.error(error);
+    });
+  }
+
+  getBtsXrpUsdArb(rateC) {
+    return fetch('https://cors-anywhere.herokuapp.com/https://www.bitstamp.net/api/v2/ticker/XRPUSD/')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        const price = responseJson.ask;
+        const result = { 'cex' : ((rateC / price) - 1) * 100 };  
+        return result;
+    })
+      .catch((error) => {
+      return { 'cex' : ((rateC / 1) - 1) * 100 }; 
       console.error(error);
     });
   }
@@ -135,16 +210,61 @@ class Tile extends React.Component {
   }
 
   getUsdTRY() {
-    return fetch('https://api.fixer.io/latest?base=USD&from=TRY&to=TRY')
+    return fetch('https://www.doviz.com/api/v1/currencies/USD/latest')
       .then((response) => response.json())
       .then((responseJson) => {
-      return responseJson.rates.TRY;
+        console.log(responseJson.buying);
+      return responseJson.buying;
+    })
+      .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  getCexXRPUSD() {
+    return fetch('https://cors-anywhere.herokuapp.com/https://cex.io/api/ticker/XRP/USD')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.bid;
+    })
+      .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  getCexXRPEUR() {
+    return fetch('https://cors-anywhere.herokuapp.com/https://cex.io/api/ticker/XRP/EUR')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.bid;
+    })
+      .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  getCexBTCEUR() {
+    return fetch('https://cors-anywhere.herokuapp.com/https://cex.io/api/ticker/BTC/EUR')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.bid;
     })
       .catch((error) => {
       console.error(error);
     });
   }
   
+  getCexBTCUSD() {
+    return fetch('https://cors-anywhere.herokuapp.com/https://cex.io/api/ticker/BTC/USD')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.bid;
+    })
+      .catch((error) => {
+      console.error(error);
+    });
+  }
+
   render() {
     
     return (
@@ -152,9 +272,6 @@ class Tile extends React.Component {
         <div class="row">
           <div class="col-sm">
             Bitstamp-BtcTurk: {this.state.btsBt}
-          </div>
-          <div class="col-sm">
-            Kraken-BtcTurk: {this.state.krkBt}
           </div>
           <div class="col-sm">
             Bitfinex-BtcTurk: {this.state.bfnBt}
@@ -166,16 +283,34 @@ class Tile extends React.Component {
             Bitstamp-Paribu: {this.state.btsPr}
           </div>
           <div class="col-sm">
-            Kraken-Paribu: {this.state.krkPr}
-          </div>
-          <div class="col-sm">
             Bitfinex-Paribu: {this.state.bfnPr}
           </div>
         </div>
         <br />
         <div class="row">
           <div class="col-sm">
-            Kraken-BtcTurk ETH: {this.state.krkBtEth}
+            Bitstamp-Cex XrpUsd: {this.state.btsCexXrpUsd}
+          </div>
+          <div class="col-sm">
+            Bitstamp-Cex XrpEur: {this.state.btsCexXrpEur}
+          </div>
+          <div class="col-sm">
+            Bitstamp-Cex BtcUsd: {this.state.btsCexBtcUsd}
+          </div>
+          <div class="col-sm">
+            Bitstamp-Cex BtcEur: {this.state.btsCexBtcEur}
+          </div>
+        </div>
+        <br />
+        <div class="row">
+          <div class="col-sm">
+            Bitfinex-Cex XrpUsd: {this.state.bfnCexXrpUsd}
+          </div>
+          <div class="col-sm">
+          Bitfinex-Cex BtcUsd: {this.state.bfnCexBtcUsd}
+          </div>
+          <div class="col-sm">
+          Bitfinex-Cex BtcEur: {this.state.bfnCexBtcEur}
           </div>
         </div>
       </div>
